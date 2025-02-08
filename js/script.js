f5b2db82dd0a8cb234e58215508fe56f461b9688
yogesh sharma <yogeshsharma2003kh@gmail.com>
2025-02-08 12:51:05 +0530

    form

--- js/script.js
+++ js/script.js
@@ -71,5 +71,69 @@ anim.from(".headi",{
 
 
 });
+// Show the form when the page loads
+window.onload = function () {
+    document.getElementById("contact-form").classList.remove("opacity-0");
+};
+
+// Function to close the form
+function closeForm() {
+    document.getElementById("contact-form").classList.add("opacity-0");
+}
+
+gsap.from(".start-form",
+    {
+        scale:0,
+        duration:2,
+        opacity:0,
+        x:1200
+    }
+)
 
 
+function init()
+{
+    gsap.registerPlugin(ScrollTrigger);
+
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector(".main"),
+  smooth: true
+});
+
+locoScroll.on("scroll", ScrollTrigger.update);
+
+ScrollTrigger.scrollerProxy(".main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  },
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
+});
+
+
+
+
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+
+ScrollTrigger.refresh();
+
+}
+init();
+window.addEventListener("load",function()
+{
+this.setTimeout(
+    function open()
+    {
+        document.querySelector(".box-form").style.display="block";
+    },1000
+)
+}
+)
+document.querySelector(".iconmost").addEventListener("click",function()
+{
+    document.querySelector(".box-form").style.display="none";
+})
